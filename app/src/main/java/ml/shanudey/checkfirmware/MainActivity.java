d140abe0ae24007495f28d8ea349007a6ffd1d87
ShanuDey <sd4shanudey@gmail.com>
2020-03-14 11:50:03 +0530

    extract timestamp from garbage
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- app/src/main/java/ml/shanudey/checkfirmware/MainActivity.java
+++ app/src/main/java/ml/shanudey/checkfirmware/MainActivity.java
@@ -9,13 +9,22 @@ import ml.shanudey.root.Utils;
 
 public class MainActivity extends AppCompatActivity {
     private static final String FIRMWARE_VERSION_FILEPATH = "/firmware/verinfo/ver_info.txt";
-
+    private String timeStamp;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         String res = Utils.readFile(FIRMWARE_VERSION_FILEPATH);
-        Log.v("result",res);
+//        Log.v("result",res);
+        for (String s : res.split(",")){
+//            Log.v("forloop ",s);
+            if (s.contains("Time_Stamp")){
+//                Log.v("line",s);
+                timeStamp = s.split(":")[1].substring(2,12);
+            }
+        }
+        Log.v("Firmware Date:",timeStamp);
     }
 }
