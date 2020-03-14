5ca4e03477aaf26be8885600a82dddc8ba3a43bc
ShanuDey <sd4shanudey@gmail.com>
2020-03-14 11:56:06 +0530

    Json request is handled with care :p
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- app/src/main/java/ml/shanudey/checkfirmware/MainActivity.java
+++ app/src/main/java/ml/shanudey/checkfirmware/MainActivity.java
@@ -5,12 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import ml.shanudey.root.Utils;
 
 public class MainActivity extends AppCompatActivity {
     private static final String FIRMWARE_VERSION_FILEPATH = "/firmware/verinfo/ver_info.txt";
+    public static final String API_URL = "https://api.myjson.com/bins/8ay66";
     private String timeStamp;
-    
+    private RequestQueue requestQueue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,5 +38,30 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         Log.v("Firmware Date:",timeStamp);
+
+        requestQueue = Volley.newRequestQueue(this);
+        parseJSON();
+
+    }
+
+    private void parseJSON(){
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, API_URL,null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String currentFirmware = response.getString(timeStamp);
+                            Log.v("current Firmware", currentFirmware);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
     }
 }
