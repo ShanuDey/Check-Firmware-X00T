c22323701f7b41f9d19cad51a2830f3f537908e9
ShanuDey <sd4shanudey@gmail.com>
2020-03-14 11:47:51 +0530

    Get local firmware timestamp Thanks to Saurabh Charde
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- app/src/main/java/ml/shanudey/checkfirmware/MainActivity.java
+++ app/src/main/java/ml/shanudey/checkfirmware/MainActivity.java
@@ -3,12 +3,19 @@ package ml.shanudey.checkfirmware;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import ml.shanudey.root.Utils;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String FIRMWARE_VERSION_FILEPATH = "/firmware/verinfo/ver_info.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String res = Utils.readFile(FIRMWARE_VERSION_FILEPATH);
+        Log.v("result",res);
     }
 }
